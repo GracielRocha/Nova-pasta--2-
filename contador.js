004a6d7e084acf46bb7c80c7f9e23621e1784724
Graciel Rocha <gracielguerreiro@gmail.com>
2022-04-28 22:04:43 -0300

    codigos js testes

---
+++ contador.js
@@ -0,0 +1,5 @@
+let i = 1;
+while (i <= 10) {
+    console.log(2*i);
+    i++;
+}
---
+++ infor.js
@@ -0,0 +1,12 @@
+let usuario =
+{
+    nome: 'Cassiano',
+    idade: 32,
+    email: 'cassiano@transformatec.com'
+}
+console.log(usuario["email"])
+console.log(usuario["nome"])
+console.log(usuario.idade)
+for (propriedade in usuario) {
+    console.log("Propriedade do usuário: " + usuario[propriedade])
+}
